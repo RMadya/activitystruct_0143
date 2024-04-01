6e5a807815a00f20665dc130bdaf5004de131fcd
RMadya <kereyoben75@gmail.com>
2024-04-01 14:00:34 +0700

    int nomor dan nama mahasiswa

--- Structur3/Structur3.cpp
+++ Structur3/Structur3.cpp
@@ -13,3 +13,10 @@ struct Mahasiswa {
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    getline(cin, mhs.nim);
+    cout << "Nama Mahasiswa : ";
+    getline(cin, mhs.nama);
